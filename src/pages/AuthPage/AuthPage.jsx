57fb5389d66608d07e1f29af219e8a6765ccb7b1
radshima <shima.akbarirad@gmail.com>
2023-12-01 10:29:42 +1100

    Small CSS chaange

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -9,11 +9,17 @@ export default function AuthPage({ setUser }) {
 
   return (
     <main className="AuthPage">
-      <div>
-        <Logo />
-        <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</h3>
+      <div className="form-container">
+        {/* <Logo /> */}
+        <h3 onClick={() => setShowLogin(!showLogin)}>
+          {showLogin ? "SIGN UP" : "LOG IN"}
+        </h3>
       </div>
-      {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
+      {showLogin ? (
+        <LoginForm setUser={setUser} />
+      ) : (
+        <SignUpForm setUser={setUser} />
+      )}
     </main>
   );
 }
