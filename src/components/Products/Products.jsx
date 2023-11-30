351d0c226f1f86333c29872f2ccbf8cb954df1ee
Greg Ells <gregells@gmail.com>
2023-12-01 12:19:51 +1300

    Bug fixes, .env removed from github.

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,37 +4,18 @@ import Product1Image from "../../assets/images/1.jpg";
 import Product2Image from "../../assets/images/2.jpg";
 import Product3Image from "../../assets/images/3.jpg";
 
-function Products({activeCat, menuItems, handleAddToOrder}) {
-  // let Products = [
-  //   {
-  //     ProductImage: Product1Image,
-  //     ProductTitle: "Lucia Linen Dress",
-  //     ProductDesc:
-  //       "Flaunting tonal teals and blues within a floral print.",
-  //     Productsize: "M",
-  //     Productprice: 279,
-  //   },
-  //   {
-  //     ProductImage: Product2Image,
-  //     ProductTitle: "Morley Mini Dress",
-  //     ProductDesc: "Utterly effervescent in a pale shade of champagne.",
-  //     Productsize: "L",
-  //     Productprice: 229,
-  //   },
-  //   {
-  //     ProductImage: Product3Image,
-  //     ProductTitle: "Shae Silk Dress",
-  //     ProductDesc:
-  //       "Slipping over the skin to create a silhouette.",
-  //     Productsize: "L",
-  //     Productprice: 299,
-  //   },
-  // ];
-
-  // menuItems={menuItems.filter(item => item.category.name === activeCat)};
-  console.log(menuItems);
+function Products({activeCat, menuItems, setCart}) {
+  // console.log(menuItems);
   
-  const items = menuItems.map((item) => <ProductItem key={item._id} productItem={item} handleAddToOrder={handleAddToOrder}/>)
+  const items = menuItems.map((item) => 
+    <ProductItem 
+      key={item._id} 
+      productItem={item} 
+      setCart={setCart}
+    />
+  );
+
+  console.log(setCart);
 
   return (
     <section id="Product">
@@ -44,9 +25,6 @@ function Products({activeCat, menuItems, handleAddToOrder}) {
         </div>
         <div className="row">
           {items}
-          {/* <ProductItem {...Products[0]} />
-          <ProductItem {...Products[1]} />
-          <ProductItem {...Products[2]} /> */}
         </div>
       </div>
     </section>
