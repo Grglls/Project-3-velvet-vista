fa1201f93ead60f39172a77b840cca5157c3c0fe
howjeejyee <146084645+howjeejyee@users.noreply.github.com>
2023-11-30 22:45:51 +0800

    update

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -13,25 +13,13 @@ export default function App() {
   const [categories, setCategories] = useState([]);
   const [price, setPrice] = useState('');
   const [searchTerm, setSearchTerm] = useState('');
-
-  // const uploadImage = async (image) => {
-	// 	const data = new FormData()
-	// 	data.append("file", image)
-	// 	data.append("upload_preset", "project-3")
-	// 	data.append("cloud_name", "dz9ufxjca")
-	// 	return fetch("https://api.cloudinary.com/v1_1/dz9ufxjca/image/upload",{
-	// 		method: "post",
-	// 		body: data
-  //   		}).then(res => res.json())
-  //     .catch(err => console.log(err))
-	// };
   
   return (
     <>
       <NavBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <main className="App">
         <Routes>
-          <Route path="/" element={<HomePage name={name} setName={setName} categories={categories} setCategories={setCategories} price={price} setPrice={setPrice} uploadImage={uploadImage} />} />
+          <Route path="/" element={<HomePage name={name} setName={setName} categories={categories} setCategories={setCategories} price={price} setPrice={setPrice} />} />
         </Routes>
       </main>
     </>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -13,37 +13,3 @@ export default function HomePage() {
     </main>
   );
 };
-
-// export default function HomePage ({uploadImage}) {
-//   const [image, setImage] = useState('');
-//   const _handleImageChange = (e) => {
-//     setImage(e.target.files[0]);
-// }
-//   const _handleSubmit = async (e) => {
-//     e.preventDefault();
-    
-//     try {
-//         if (image) {
-//             const data = await uploadImage(image);
-//             console.log(data);
-//             // newTask.image = data.url;
-//         } else {
-//             setImage('');
-//         }
-//     } catch (error) {
-//         console.log(error);
-//     }
-//   }
-
-  //   return (
-  //     <main className="HomePage">
-  //       <div>
-  //           Home Page Test
-  //       </div>
-  //       <form onSubmit={_handleSubmit}>
-  //         <input type="file" onChange={_handleImageChange} />
-  //         <button>Submit</button>
-  //       </form>
-  //     </main>
-  //   );
-  // }
